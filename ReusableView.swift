340796c573c112dd14ab8267d4ada0590803b958
xuyi <xuyi@guoxiaomei.com>
2017-12-26 14:21:30 +0800

    add reusable view

---
+++ ReusableView.swift
@@ -0,0 +1,90 @@
+//
+//  ReusableView.swift
+//  ReusableView
+//
+//  Created by seanxux on 15/12/2017.
+//  Copyright © 2017 seanxux. All rights reserved.
+//
+
+import Foundation
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+}
+
+extension UICollectionReusableView: ReusableView {}
+extension UITableViewCell: ReusableView {}
+
+protocol NibLoadableView: class {
+    static var nibName: String { get }
+}
+
+extension NibLoadableView where Self: UIView {
+    static var nibName: String {
+        return NSStringFromClass(self).components(separatedBy: ".").last!
+    }
+}
+
+extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UITableViewCell>(_: T.Type) where T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+}
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UICollectionReusableView>(_: T.Type, forSupplementaryViewOfKind kind: String) {
+        register(T.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UICollectionReusableView>(_: T.Type, forSupplementaryViewOfKind kind: String) where T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier,
+                                             for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+
+    func dequeueReusableSupplementaryView<T: UICollectionReusableView>(ofKind kind: String, for indexPath: IndexPath) -> T {
+        guard let view = dequeueReusableSupplementaryView(ofKind: kind,
+                                                          withReuseIdentifier: T.defaultReuseIdentifier,
+                                                          for: indexPath) as? T else {
+            fatalError("Could not dequeue supplementary with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return view
+    }
+}
